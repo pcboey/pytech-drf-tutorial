4b592168932c4f320d1a73ef8b1d15571129bc6b
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-20 15:26:08 +0800

    Use generic CBVs, save a lot of code

--- snippets/views.py
+++ snippets/views.py
@@ -1,38 +1,20 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 #########################################################################################
-## pytech-drf-tutorial step-5
-## Using Mixins
-## See http://www.django-rest-framework.org/tutorial/3-class-based-views/#using-mixins
+## pytech-drf-tutorial step-6
+## Using generic class based views
+## See http://www.django-rest-framework.org/tutorial/3-class-based-views/#using-generic-class-based-views
+
+## WE REALLY SAVE A LOT OF CODE!!!
 #########################################################################################
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
