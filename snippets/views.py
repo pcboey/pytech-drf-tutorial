7733897215097f1c0deb6b48d8d9adf55d2942f3
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-21 11:18:12 +0800

    Added generic CBVS for User

--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,9 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+
 from rest_framework import generics
 
-#########################################################################################
-## pytech-drf-tutorial step-6
-## Using generic class based views
-## See http://www.django-rest-framework.org/tutorial/3-class-based-views/#using-generic-class-based-views
-
-## WE REALLY SAVE A LOT OF CODE!!!
-#########################################################################################
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -18,3 +13,16 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+#########################################################################################
+## pytech-drf-tutorial step-7
+## See http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-endpoints-for-our-user-models
+## Use ListAPIView and RetrieveAPIView generic class based views to add some views
+#########################################################################################
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
