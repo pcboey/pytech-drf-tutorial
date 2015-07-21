6c8fde72ab3fdcdd9de570c48bbcacdde74ded8c
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-21 11:15:24 +0800

    Added endpoints for User model

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,15 +1,24 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from .models import Snippet
 
 ################################################################################
-## Using ModelSerializers
-## No Authentication and Permissions
-## Anyone can edit or delete code snippets
-## See http://www.django-rest-framework.org/tutorial/1-serialization/   Using ModelSerializers
+## Authentication & Permissions
+## Adding endpoints for our User models
+## 
+## See http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-endpoints-for-our-user-models
 ################################################################################
  
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
